d4fc040c79e541ae3f0310f782e3590b23a89084
Max W. Portocarrero <mxportocarrero@gmail.com>
2018-02-15 09:12:19 -0500

    Puntos Colineales

--- main.cpp
+++ main.cpp
@@ -21,7 +21,9 @@ using namespace cv;
 
 //*****Control Points Vector********//
 std::vector<Point2f> trackedPoints;
-int numTrackedItems = 6;
+std::vector<Point2f> ControlPoints;
+bool patternWasFound;
+int numTrackedItems = 20;
 bool isTracking = false;
 
 /**Variables for Global Threshold**/
@@ -126,7 +128,135 @@ vector<Point2f> getControlPoints(const vector<Point2f> & centers){
 
     return v;    
 }
+//=============================================================
+//================ FUNCION SUPERIMPORTANTE ====================
+//=============================================================
+// Si la funcion tiene exito devulve true, mas la modificacion del vector de entrada
+// Con el numero de filas y columnas especificadas
+bool isColinear(Vec4f line, Point2f point){
+    // Evaluamos para x e y
+    float tx = (point.x - line[2]) / line[0];
+    float ty = (point.y - line[3]) / line[1];
 
+    if(abs(tx-ty) < 10.0f) return true;
+    return false;
+}
+
+bool FindRingPattern(vector<Point2f> &probableCPs,int num_rows,int num_cols){
+    int n = probableCPs.size();
+    std::vector<Vec4f> lines;
+    for(int i = 0; i < probableCPs.size();i++){
+        for(int j = i+1; j < probableCPs.size();j++){
+            std::vector<Point2f> probableLinePoints;
+            Vec4f tmpLine;
+            probableLinePoints.push_back(probableCPs[i]);probableLinePoints.push_back(probableCPs[j]);
+            fitLine(probableLinePoints,tmpLine, CV_DIST_L2, 0, 0.01,0.01);
+            lines.push_back(tmpLine);
+
+            float vx = tmpLine[0],vy = tmpLine[1], x0 = tmpLine[2],y0 = tmpLine[3];
+            float m = 80.0;
+            //line(frame, Point2f(x0-m*vx, y0-m*vy), Point2f(x0+m*vx, y0+m*vy),Scalar(0,255,0));
+        }
+    }
+
+    std::vector<Vec4f> selected;
+    std::vector<int> already;
+    for(int i = 0; i < lines.size();i++){
+        for(int j = i+1; j < lines.size();j++){
+            float a[2] = {lines[i][0],lines[i][1]};
+            float b[2] = {lines[j][0],lines[j][1]};
+
+            cv::Mat AA(1,2,CV_32FC1,a);
+            cv::Mat BB(1,2,CV_32FC1,b);
+
+            int count_1 = 0, count_2 = 0;
+
+            for(int k = 0; k < probableCPs.size();k++){
+                if(isColinear(lines[i],probableCPs[k])) count_1++;
+                if(isColinear(lines[j],probableCPs[k])) count_2++;
+            }
+
+
+            if(AA.dot(BB) < 0.1 && count_1 == 4 && count_2 == 5 && find(already.begin(),already.end(),i) == already.end()){
+                selected.push_back(lines[i]);
+                selected.push_back(lines[j]);
+                float vx = lines[i][0],vy = lines[i][1], x0 = lines[i][2],y0 = lines[i][3];
+                float m = 80.0;
+                line(frame, Point2f(x0-m*vx, y0-m*vy), Point2f(x0+m*vx, y0+m*vy),Scalar(0,255,250));
+                vx = lines[j][0],vy = lines[j][1], x0 = lines[j][2],y0 = lines[j][3];
+                line(frame, Point2f(x0-m*vx, y0-m*vy), Point2f(x0+m*vx, y0+m*vy),Scalar(0,255,250));
+                already.push_back(i);
+
+            }
+        }
+    }
+    /*
+    for(int i = 0; i < selected.size(); i++){
+        cout << selected[i] << endl; 
+    }
+    */
+
+    std::vector<Vec4f> selected_positive;
+    std::vector<Vec4f> selected_negative;
+    for(int i = 0; i < selected.size();i++){
+        float m = selected[i][1] / selected[i][0];
+        //cout << m << endl;
+        if(m > 0) selected_positive.push_back(selected[i]);
+        else selected_negative.push_back(selected[i]);
+    }
+    std::vector<float> x_negative;
+    for(int i = 0; i < selected_negative.size();i++){
+        float t = -selected_negative[i][3] / selected_negative[i][1];
+        float x = selected_negative[i][2] + t * selected_negative[i][0];
+        x_negative.push_back(x);
+    }
+
+    std::vector<float> x_positive;
+    for(int i = 0; i < selected_positive.size();i++){
+        float t = -selected_positive[i][3] / selected_positive[i][1];
+        float x = selected_positive[i][2] + t * selected_positive[i][0];
+        x_positive.push_back(x);
+    }
+
+    //Hallamos las cuatro lineas horizontales
+    std::vector<int> alr;
+    for(int y= 0 ; y < 4; y++){
+        //Bucle -->
+        //Creamos un vector con los indices ordenados
+        int max_index; float max = -1000000.0;
+        for(int i = 0; i < selected_positive.size();i++){
+            //cout << x_positive[i] << " ";
+            if(max < x_positive[i] && find(alr.begin(), alr.end(),i) == alr.end()){
+                max = x_positive[i];
+                max_index = i;
+            }
+        }
+        alr.push_back(max_index);
+        //cout << "MAXINDEX: " << max_index;
+
+        //Get Colinear Points with this line
+        std::vector<Point2f> row;
+        for(int i = 0; i < probableCPs.size();i++){
+            if(isColinear(selected_positive[max_index],probableCPs[i]))
+                row.push_back(probableCPs[i]);
+        }
+        cout << row.size()<<endl;
+
+        sort(row.begin(), row.end(),cmpx);
+        for(int i = 0; i < row.size();i++){
+            //circle(frame,row[i],5,Scalar(255,0,255),3,8);
+            ControlPoints.push_back(row[i]);
+        }
+
+    }
+
+    //Calculando el valor de x cuando y es igual a 0
+    return true;
+}// fin de funcion importante
+
+//=============================================================
+//================ FUNCION SUPERIMPORTANTE ====================
+//=============================================================
 
 void EllipsisDetection(){
     vector<vector<Point>> contours;
@@ -181,35 +311,29 @@ void EllipsisDetection(){
                 int child_index = hierachy[i][2];
                 if(hierachy[child_index][0] == -1 && hierachy[child_index][1] == -1 && hierachy[child_index][2] == -1){
                     selected.push_back(minEllipse[i]);
+                    selected.push_back(minEllipse[child_index]);
                     //putText(Ellipsis,to_string(i),Point(c_x,c_y),FONT_HERSHEY_SIMPLEX,0.4,Scalar(255,0,0),1,CV_AA);
                     //cout << selected.size() << endl;
                     //cout <<"width: "<< w <<" height: "<< h << endl;
                     //cout <<"dif: "<< abs(dif) <<" Semiperimetro: "<< sum << " area: " << area<< endl; 
                     ellipse( Ellipsis, minEllipse[i], Scalar(0,0,255), 1, 8 );
+                    ellipse( Ellipsis, minEllipse[child_index], Scalar(0,0,255), 1, 8 );          
                 }
-            }
-         
-            if( hierachy[i][0] == -1 && hierachy[i][1] == -1 && hierachy[i][2] == -1){
-                selected.push_back(minEllipse[i]);
-                //putText(Ellipsis,to_string(i),Point(c_x,c_y),FONT_HERSHEY_SIMPLEX,0.4,Scalar(255,0,0),1,CV_AA);
-                //cout << selected.size() << endl;
-                //cout <<"width: "<< w <<" height: "<< h << endl;
-                //cout <<"dif: "<< abs(dif) <<" Semiperimetro: "<< sum << " area: " << area<< endl; 
-                ellipse( Ellipsis, minEllipse[i], Scalar(0,0,255), 1, 8 );          
             }
         }
         
         
     }
 
+
+    //Extraemos los centros de todas las elipses Seleccionadas
     cout << "Number Selected Ellipsises: " << selected.size() << endl;
     vector<Point2f> centers;
     for( int i = 0; i < selected.size(); i++ ){
-        centers.push_back(selected[i].center);
-        //cout << "x:"<< centers[i].x << ", y:" << centers[i].y << endl;
-        //putText(Ellipsis,to_string(i),Point(centers[i].x,centers[i].y),FONT_HERSHEY_SIMPLEX,0.4,Scalar(255,0,0),1,CV_AA);      
+        centers.push_back(selected[i].center);      
     }
 
+    //Eliminamos Duplicados y nos quedamos con el promedio de Centros similares('permanezcan a un conjunto de elipses')
     vector<Point2f> CPs = getControlPoints(centers);
     cout << "Number of Control Points: "<< CPs.size() <<endl;
 
@@ -217,6 +341,8 @@ void EllipsisDetection(){
         circle(Ellipsis,CPs[i],1,Scalar(0,0,255),3,8);
     }
 
+
+    //Secuencia de Tracking
     if(isTracking){
         std::vector<float> distances;
         for(int k = 0; k < numTrackedItems;k++){
@@ -245,24 +371,22 @@ void EllipsisDetection(){
 
         // Standard deviation standard algorithm
         std::vector<float> var(numTrackedItems);
-        for (int i = 0; i < numTrackedItems; ++i)
-        {
+
+        for (int i = 0; i < numTrackedItems; ++i){
             var[i] = (dmean - distances[i]) * (dmean - distances[i]);
         }
-        for (int i = 0; i < numTrackedItems; ++i)
-        {
+
+        for (int i = 0; i < numTrackedItems; ++i){
             dstddev += var[i];
         }
         dstddev = sqrt(dstddev / (float)numTrackedItems);
-        //cout.precision(6);
         std::cout << "Mean: " << dmean << "   StdDev: " << dstddev << std::endl;
 
-        if(dstddev >3.0f){
+        //Aumentar validaciones en esta zona
+        if(dstddev >5.0f)
             isCorrect = false;
-        }
 
-
-
+        //Si no es correcto el mandar señal para tratar de capturar el tracking
         if(!isCorrect){
             cout << "Couldnt keep tracking\n";
             isTracking = false;
@@ -270,16 +394,41 @@ void EllipsisDetection(){
 
     }
     else{
-        cout << "StartTracking\n";
-        trackedPoints.clear();
-        for(int i = 0; i < numTrackedItems; i++){
-            trackedPoints.push_back(CPs[i]);
+        cout << "Start Tracking\n";
+        // Buscamos encontrar el patron, devolvemos solo el numero correspondiente de nodos
+        // Ademas Ordenamos los nodos, primero por fila, luego por columna
+        patternWasFound = FindRingPattern(CPs,4,5);
+        //  patternWasFound = false;
+
+
+
+        //Esta parte del codigo debe enviar 20 puntos Ordenados y en grilla hacia TrackedPoints
+        //En cualquier otro caso debe pasar al siguiente frame y tratar otra vez
+        //El ordenamiento a pasar es el siguiente
+        /**
+        0 1 2 3 4
+        5 6 7 8 9
+        10 11 12 13 14
+        15 16 17 18 19
+        **/
+        if(patternWasFound){
+            trackedPoints.clear();
+            for(int i = 0; i < numTrackedItems; i++){
+                trackedPoints.push_back(ControlPoints[i]);
+            }
+
+            isTracking = true;
         }
-        isTracking = true;
     }
-    if(isTracking)
-        for(int i = 0; i < numTrackedItems; i++)
-            circle(frame, trackedPoints[i],1,Scalar(0,0,255),3,8);
+
+    //Finalmente Dibuja el vector preordenado y verificado
+    if(isTracking){
+
+        //for(int i = 0; i < numTrackedItems; i++)
+        //    circle(frame, trackedPoints[i],1,Scalar(0,0,255),3,8);
+        drawChessboardCorners(frame, Size(5,4), trackedPoints, patternWasFound);
+    }
+
 
     /**
 
@@ -401,6 +550,7 @@ int main(){
             finish = 0;
             break;
         }
+        
 
         //For Video Capture
         //int key = waitKey(10);
