54ebe8be0b6cc65894f77ba6c31ebf52403802b6
Max W. Portocarrero <mxportocarrero@gmail.com>
2018-03-02 00:20:31 -0500

    Falta aumentar los otros inputs y calcular mas metricas para el error. Aumentar nuestro algoritmo a las opciones

--- calibration/main.cpp
+++ calibration/main.cpp
@@ -1,5 +1,5 @@
 /**
-g++ -std=c++11 main.cpp addfunctions.cpp `pkg-config opencv --cflags --libs` -o main && ./main
+g++ -std=c++11 -O3 main.cpp addfunctions.cpp `pkg-config opencv --cflags --libs` -o main && ./main
 **/
 
 #include "includes.h"
@@ -15,11 +15,12 @@ g++ -std=c++11 main.cpp addfunctions.cpp `pkg-config opencv --cflags --libs` -o 
 /** Global Variables **/
 cv::Mat frame;
 int patternType = CHESSBOARD; // Tipo de Patrón empleado (Ver Enum en includes)
-cv::Size imgPixelSize = Size(480,640); // Tamaño de la imagen
+cv::Size imgPixelSize = Size(640,480); // Tamaño de la imagen
 cv::Size patternSize = Size(6,9); // Varia dependiendo del tipo de patron que usamos
-float squareSize = 0.03; // Separacion Real(en m) entre los puntos detectados
+float squareSizes[] = {0.02315,0.02315,0.02315,0.02315}; // Separacion Real(en m) entre los puntos detectados
 // el Size(x,y) .. x: numero de filas, y: numero de columnas
-int noImages = 12; // Number of Images used for calibration
+int noImages = 10; // Number of Images used for calibration
+int sample_FPS = 50; // trata de tomar una muestra cierto FPS
 int mode = 0;
 
 std::vector< std::vector<cv::Point3f> > objPoints; // Puntos de nuestro objeto(Patron de calibracion)
@@ -38,6 +39,8 @@ int main(){
 #else
     cv::VideoCapture cap(0); // --> For video Capture
     cap.set(cv::CAP_PROP_FPS,60); // ---> Cantidad de FPS caputrados por la camara
+    cap.set(CV_CAP_PROP_FRAME_WIDTH,640);
+    cap.set(CV_CAP_PROP_FRAME_HEIGHT,480);
 #endif
 
     if(!cap.isOpened()){
@@ -54,7 +57,6 @@ int main(){
     std::vector<cv::Mat> rvecs,tvecs; //Vectores de rotacion y de traslacion para para frame
 
     int key;
-
     int counter = 0;
     while(true){
 
@@ -77,62 +79,64 @@ int main(){
     	switch(mode){
 
     		case DETECT_MODE:{
-    			if(counter % 10 == 0){
+    			//Esta funcion llena nuestro objPoints vector of vectors, suponiendo una superficie plana
+	    		objPoints.resize(1);
+				calcBoardCornerPositions(patternSize,squareSizes[CHESSBOARD],objPoints[0],patternType);
+				objPoints.resize(noImages,objPoints[0]);
 
-	    			//Esta funcion llena nuestro objPoints vector of vectors, suponiendo una superficie plana
-	    			objPoints.resize(1);
-					calcBoardCornerPositions(patternSize,squareSize,objPoints[0],patternType);
-					objPoints.resize(noImages,objPoints[0]);
+	    		bool found = false;
 
-	    			bool found = false;
+			    std::vector<cv::Point2f> PointBuffer;
 
-			    	std::vector<cv::Point2f> PointBuffer;
-
-			    	switch(patternType){
-			    		case CHESSBOARD:
-			    			found = cv::findChessboardCorners(frame, patternSize,PointBuffer);
-			    			break;
-			    		case RINGS_GRID:
-			    			found = findRingGridPattern(frame,patternSize,PointBuffer);
-			    			break;
-			    		default:
-			    			found = false;
-			    			break;
-			    	}
-
+			    switch(patternType){
+			    	case CHESSBOARD:
+			    		found = cv::findChessboardCorners(frame, patternSize,PointBuffer);
+			    		break;
+			    	case RINGS_GRID:
+			    		found = findRingGridPattern(frame,patternSize,PointBuffer);
+			    		break;
+			    	default:
+			    		found = false;
+			    		break;
+			    }
 			    	// Si encontramos el patron --> lo dibujamos en el frame
-			    	if(found){
-			    		// Paso adicional para refinar esquina en el caso de Chessboard
-			    		if(patternType == CHESSBOARD){
-			    			cv::Mat gray;
-			    			cv::cvtColor(frame, gray, cv::COLOR_BGR2GRAY);
-			    			cv::cornerSubPix(gray, PointBuffer, Size(11,11), Size(-1,-1),criteria);
-			    		}
-			    		cv::drawChessboardCorners(frame, patternSize, PointBuffer,found);
-
+		    	if(found){
+		    		// Paso adicional para refinar esquina en el caso de Chessboard
+		    		if(patternType == CHESSBOARD){
+		    			cv::Mat gray;
+		    			cv::cvtColor(frame, gray, cv::COLOR_BGR2GRAY);
+		    			cv::cornerSubPix(gray, PointBuffer, Size(11,11), Size(-1,-1),criteria);
+		    		}
+		    		cv::drawChessboardCorners(frame, patternSize, PointBuffer,found);
 			    		//Agregamos el patrón encontrado a nuestros imgPoints
-			    		imgPoints.push_back(PointBuffer);
+                    if(counter % sample_FPS == 0){
+		    		    imgPoints.push_back(PointBuffer);
+                        cout << "Imagen Capturada. Imagenes Totales: " << imgPoints.size() << endl;
+                    }
+		    	}
 
-			    	}
-
-
-	    			// verificamos si ya encontramos suficientes imagenes
-	    			// caso contrario esperamos una cantidad de tiempo para tratar de capturar otra imagen
-	    			if(imgPoints.size() == noImages)
-	    				mode++;
-	    			else
-	    				key = cv::waitKey(1); // Tiempo de espera 1000ms
-    			}
+    			// verificamos si ya encontramos suficientes imagenes
+    			// caso contrario esperamos una cantidad de tiempo para tratar de capturar otra imagen
+    			if(imgPoints.size() == noImages)
+    				mode++;
+    			else
+    				key = cv::waitKey(1); // Tiempo de espera 1000ms
     			break;
     		}
+
     		case CALIBRATION_MODE:{
     			rms = calibrateCamera(objPoints, imgPoints, imgPixelSize, cameraMatrix, distCoeffs, rvecs, tvecs);
     			cout << "El error de reproyeccion obtenido fue de " << rms << endl;
-    			mode++;
-    			break;
-    		}
-    		case UNDISTORTION_MODE:{
+                cout << "Matriz Intrinseca" << endl << cameraMatrix << endl;
+                cout << "Coeficientes de distorsion" << endl << distCoeffs << endl;
     			cout << "Empezando la InDistorsion \n";
+                mode++;
+                break;
+            }
+
+            case UNDISTORTION_MODE:{
+                cv::Mat temp = frame.clone();
+                cv::undistort(temp,frame,cameraMatrix,distCoeffs);
     			key = waitKey(1);
     			break;
     		}
